349db77d404035bd7b6995cb904a792025372138
chleck <dmitrychleck@gmail.com>
2012-07-22 09:47:31 +0200

    Changes in test suite.

--- test/test.js
+++ test/test.js
@@ -9,18 +9,21 @@ describe('Test suite for vchains validation library', function(){
       var v = validate();
     })
 
-    it('Add custom validation method by API call', function(){
+    it('Add custom validation method by API call ( test() )', function(){
       vchains.use('test', function(msg){
         if(this.value.length) return msg || 'Error';
       });
     })
 
-    it('Use custom validation method added by API call (valid)', function(){
-      validate('').test().msg().should.equal('');
-    })
-
-    it('Use custom validation method added by API call (error)', function(){
-      validate(' ').test().msg().should.equal('Error');
+    it('Add multiple custom validation method by API call ( test1() & test2() )', function(){
+      vchains.use({
+        'test1': function(msg){
+          if(this.value.length != 1) return msg || 'Error';
+        },
+        'test2': function(msg){
+          if(this.value.length != 2) return msg || 'Error';
+        }
+      });
     })
 
   })
@@ -33,6 +36,26 @@ describe('Test suite for vchains validation library', function(){
 
     it('isInt() (error)', function(){
       validate('a123').isInt().msg().should.equal('Bad int value');
+    })
+
+  })
+
+  describe('Custom validators:', function(){
+
+    it('Use custom validation method added by API call (valid)', function(){
+      validate('').test().msg().should.equal('');
+    })
+
+    it('Use custom validation method added by API call (error)', function(){
+      validate(' ').test().msg().should.equal('Error');
+    })
+
+    it('Use multiple custom validation method added by API call (valid)', function(){
+      validate('1').test1().msg().should.equal('');
+    })
+
+    it('Use multiple custom validation method added by API call (error)', function(){
+      validate('1').test2().msg().should.equal('Error');
     })
 
   })
