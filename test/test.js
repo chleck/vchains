0f83f7ddcb9d3d5383a52d168a383d4524efdcfa
Dmitry Chleck <dmitrychleck@gmail.com>
2013-08-14 02:16:09 +0400

    Update for test suite.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 vchains = require('../index.js');
 validate = vchains.validate;
 
-var v;
+var v
+  , good  = 'good' // Success message for validators tests
+  , bad   = 'bad'; // Error message for validators tests
+
 
 describe('Test suite for vchains validation library', function(){
 
@@ -9,29 +12,117 @@ describe('Test suite for vchains validation library', function(){
 
     it('Create validation object', function(){
       v = validate();
-      var t = typeof v;
-      t.should.equal('object');
+      (typeof v).should.equal('object');
     })
 
     it('Extend validation object', function(){
-      vchains.extend('ext', function(){
-        return 'ext';
+      vchains.extend('myMethod', function(){
+        return 'Me!';
       });
-      validate().ext().should.equal('ext');
+      validate().myMethod().should.equal('Me!');
     })
 
   })
 
   describe('Validators:', function(){
 
-    it('isInt()', function(){
-      validate('123').isInt().msg().should.equal('');
-      validate('a123').isInt().msg().should.equal('Bad int value');
+    it('invalid()', function(){
+      validate('123').invalid(bad).msg().should.equal(bad);
     })
 
     it('is()', function(){
-      validate('asdfgh').is(/^asd/).msg().should.equal('');
-      validate('qasdfgh').is(/^asd/).msg().should.equal('Bad value');
+      validate('asdfgh', good).is(/^asd/).msg().should.equal(good);
+      validate('qasdfgh').is(/^asd/, bad).msg().should.equal(bad);
+    })
+
+    it('isNot()', function(){
+      validate('qasdfgh', good).isNot(/^asd/).msg().should.equal(good);
+      validate('asdfgh').isNot(/^asd/, bad).msg().should.equal(bad);
+    })
+
+    it('isEmail()', function(){
+      validate('asd@qwe.ru', good).isEmail().msg().should.equal(good);
+      validate('asd').isEmail(bad).msg().should.equal(bad);
+    })
+
+    it('isUrl()', function(){
+      validate('http://dot.net/dir/index.html?a=123&b=321', good).isUrl().msg().should.equal(good);
+      validate('asd').isUrl(bad).msg().should.equal(bad);
+    })
+
+    it('isIp()', function(){
+      validate('127.0.0.1', good).isIp().msg().should.equal(good);
+      validate('365.1.1.1').isIp(bad).msg().should.equal(bad);
+    })
+
+    it('isNum()', function(){
+      validate('-0987654321', good).isNum().msg().should.equal(good);
+      validate('-').isNum(bad).msg().should.equal(bad);
+    })
+
+    it('isAlpha()', function(){
+      validate('Hello', good).isAlpha().msg().should.equal(good);
+      validate('asd1').isAlpha(bad).msg().should.equal(bad);
+    })
+
+    it('isAlphanum()', function(){
+      validate('Hello123', good).isAlphanum().msg().should.equal(good);
+      validate('Hello 123').isAlphanum(bad).msg().should.equal(bad);
+    })
+
+    it('isInt()', function(){
+      validate('123', good).isInt().msg().should.equal(good);
+      validate('a123').isInt(bad).msg().should.equal(bad);
+    })
+
+    it('isFloat()', function(){
+      validate('-123.123E+12', good).isFloat().msg().should.equal(good);
+      validate('a123').isFloat(bad).msg().should.equal(bad);
+    })
+
+    it('isLt()', function(){
+      validate('123', good).isLt(124).msg().should.equal(good);
+      validate('123').isLt(122, bad).msg().should.equal(bad);
+    })
+
+    it('isLE()', function(){
+      validate('123', good).isLE(123).msg().should.equal(good);
+      validate('123').isLE(122, bad).msg().should.equal(bad);
+    })
+
+    it('isGt()', function(){
+      validate('123', good).isGt(122).msg().should.equal(good);
+      validate('123').isGt(124, bad).msg().should.equal(bad);
+    })
+
+    it('isGE()', function(){
+      validate('123', good).isGE(123).msg().should.equal(good);
+      validate('123').isGE(124, bad).msg().should.equal(bad);
+    })
+
+    it('isLen()', function(){
+      validate('123', good).isLen(0, 3).msg().should.equal(good);
+      validate('123').isLen(0, 2, bad).msg().should.equal(bad);
+    })
+
+    it('isEq()', function(){
+      validate('123', good).isEq(123).msg().should.equal(good);
+      validate('123').isEq(' 123', bad).msg().should.equal(bad);
+    })
+
+    it('isIn()', function(){
+      validate('a', good).isIn([ 'a', 'b', 'c' ]).msg().should.equal(good);
+      validate('z').isIn([ 'a', 'b', 'c' ], bad).msg().should.equal(bad);
+    })
+
+    it('isNotIn()', function(){
+      validate('z', good).isNotIn([ 'a', 'b', 'c' ]).msg().should.equal(good);
+      validate('a').isNotIn([ 'a', 'b', 'c' ], bad).msg().should.equal(bad);
+    })
+
+    it('isCard()', function(){
+      validate('4276-8010-1016-5090', good).isCard().msg().should.equal(good);
+      validate('1234-5678-9011-1213').isCard(bad).msg().should.equal(bad);
     })
 
   })
